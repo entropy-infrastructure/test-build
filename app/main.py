ac57e74cb7b9367ccff2504c5eac7f7350d5edfb
Mihir <mihir1003@gmail.com>
2023-10-04 21:45:41 -0700

    openapi

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+import openai
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+    return {"message": completion.choices[0].message.content}
